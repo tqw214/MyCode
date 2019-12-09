ee0deca075ec05f4457b049affff85ea154e68f9
tqw214 <tqw214@163.com>
2019-12-09 21:54:03 +0800

    commit

--- app/src/main/java/com/viger/mycode/MainActivity.java
+++ app/src/main/java/com/viger/mycode/MainActivity.java
@@ -1,8 +1,8 @@
 package com.viger.mycode;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
