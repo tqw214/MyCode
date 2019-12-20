231a7b3f3ae0393797944a7df56bc28127fdd575
tqw214 <tqw214@163.com>
2019-12-20 22:59:28 +0800

    commit

--- app/src/test/java/com/viger/mycode/ExampleUnitTest.java
+++ app/src/test/java/com/viger/mycode/ExampleUnitTest.java
@@ -2,7 +2,9 @@ package com.viger.mycode;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -14,4 +16,54 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void test01() {
+//        int num1 = 1;
+//        int num2 = 1;
+//        int result = 0;
+//        int n = 6;
+//        for(int i=3;i<=n;i++) {
+//            result = num1+num2;
+//            num1 = num2;
+//            num2 = result;
+//        }
+//        System.out.println("this result = " + result);
+
+
+        //int result = add(6);
+        //System.out.println("this result = " + result);
+
+        //files(new File("D:\\BaiduYunDownload"));
+
+        paixu01();
+
+    }
+
+    public void paixu01() {
+        int[] array = {5,3,7,8,2,9,4,6};
+    }
+
+    public int add(int n) {
+        System.out.println("n = " + n);
+        if(n == 1 || n== 2) {
+            return 1;
+        }else {
+            return add(n-1) + add(n-2);
+        }
+    }
+
+    public void files(File file) {
+        File[] files = file.listFiles();
+        for (File f : files) {
+            if(f.isFile()) {
+                System.out.println(f.getName());
+            }else {
+                files(f);
+            }
+        }
+    }
+
+
+
 }
