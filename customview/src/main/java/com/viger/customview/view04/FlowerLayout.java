688a76427377866841f185bb07f6c96a4593794b
tqw214 <tqw214@163.com>
2020-01-14 22:29:50 +0800

    commit

--- customview/src/main/java/com/viger/customview/view04/FlowerLayout.java
+++ customview/src/main/java/com/viger/customview/view04/FlowerLayout.java
@@ -6,10 +6,26 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class FlowerLayout extends ViewGroup {
 
-    private ArrayList<View> mViews;
+    private List<Integer> listLineHeight;
+    private List<List<View>> listLineView;
+
+    public static class LineView {
+        private int top;
+        private int left;
+        private int right;
+        private int bottom;
+
+        public LineView(int t, int l, int r, int b) {
+            this.top = t;
+            this.left = l;
+            this.right = r;
+            this.bottom = b;
+        }
+    }
 
     public FlowerLayout(Context context) {
         this(context,null);
@@ -25,20 +41,105 @@ public class FlowerLayout extends ViewGroup {
     }
 
     private void init() {
-        mViews = new ArrayList<View>();
+        listLineHeight = new ArrayList<Integer>();
+        listLineView = new ArrayList<List<View>>();
     }
 
-    public void addView(View view) {
-        mViews.add(view);
-    }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        //super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        int resultWidthSieze = 0;
+        int resultHeightSize = 0;
+
+        if(widthMode == MeasureSpec.EXACTLY && heightMode == MeasureSpec.EXACTLY) {
+            resultWidthSieze = widthSize;
+            resultHeightSize = heightSize;
+        }else {
+
+            int childCount = getChildCount();
+
+            int currentLineWidth = 0;
+            int currentLineHeight = 0;
+
+            if(widthMode == MeasureSpec.AT_MOST || heightMode == MeasureSpec.AT_MOST) {
+
+                //等于所有子view的宽
+                //每一行的viwe
+
+                List<View> lineView = new ArrayList<View>();
+
+                for(int i=0;i<childCount;i++) {
+
+                    View child = getChildAt(i);
+                    measureChild(child, widthMeasureSpec, heightMeasureSpec);
+                    ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) child.getLayoutParams();
+                    int leftMargin = params.leftMargin;
+                    int rightMargin = params.rightMargin;
+                    int topMargin = params.topMargin;
+                    int bottomMargin = params.bottomMargin;
+
+                    int childWidth = child.getMeasuredWidth() + leftMargin + rightMargin;
+                    int childHeight = child.getMeasuredHeight() + topMargin + bottomMargin;
+
+                    if(currentLineWidth + childWidth > widthSize) {
+                        resultWidthSieze = Math.max(resultWidthSieze, currentLineWidth);
+                        resultHeightSize += currentLineHeight;
+
+                        listLineHeight.add(currentLineHeight);
+                        listLineView.add(lineView);
+
+                        currentLineWidth = childWidth;
+                        currentLineHeight = childHeight;
+
+                        lineView = new ArrayList<View>();
+                        lineView.add(child);
+
+
+                    }else {
+                        currentLineWidth += childWidth;
+                        currentLineHeight = Math.max(currentLineHeight, childHeight);
+                        lineView.add(child);
+
+                    }
+
+                }
+            }
+        }
+        setMeasuredDimension(resultWidthSieze, resultHeightSize);
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new MarginLayoutParams(getContext(), attrs);
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-
+        int left,top,right,bottom;
+        int currentLef = 0;
+        int currentTop = 0;
+        for(int i=0;i<listLineView.size();i++) {
+            List<View> lineView = listLineView.get(i);
+            for(int j=0;j<lineView.size();j++) {
+                View view = lineView.get(j);
+                MarginLayoutParams params = (MarginLayoutParams) view.getLayoutParams();
+                left = currentLef + params.leftMargin;
+                top = currentTop + params.topMargin;
+                right = left + view.getMeasuredWidth() + params.rightMargin;
+                bottom = top + view.getMeasuredHeight() + params.bottomMargin;
+                view.layout(left, top, right, bottom);
+                currentLef += view.getMeasuredWidth() + params.leftMargin + params.rightMargin;
+            }
+            currentLef = 0;
+            currentTop += listLineHeight.get(i);
+        }
+        listLineView.clear();
     }
 }
--- customview/src/main/java/com/viger/customview/view04/View04Activity.java
+++ customview/src/main/java/com/viger/customview/view04/View04Activity.java
@@ -1,6 +1,8 @@
 package com.viger.customview.view04;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +11,47 @@ import com.viger.customview.R;
 
 public class View04Activity extends AppCompatActivity {
 
+    private FlowerLayout flowerLayout;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layout_04);
+        flowerLayout = findViewById(R.id.flowerLayout);
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        TextView textView1 = new TextView(this);
+        textView1.setText("哈哈哈");
+        textView1.setLayoutParams(params);
+
+
+        TextView textView2 = new TextView(this);
+        textView2.setText("粉色峰会上符合是否合适");
+        textView2.setLayoutParams(params);
+
+        TextView textView3 = new TextView(this);
+        textView3.setText("粉色方式");
+        textView3.setLayoutParams(params);
+
+        TextView textView4 = new TextView(this);
+        textView4.setText("粉色方式");
+        textView4.setLayoutParams(params);
+
+        TextView textView5 = new TextView(this);
+        textView5.setText("粉色方式");
+        textView5.setLayoutParams(params);
+
+        TextView textView6 = new TextView(this);
+        textView6.setText("粉色方式");
+        textView6.setLayoutParams(params);
+
+        flowerLayout.addView(textView1);
+        flowerLayout.addView(textView2);
+        flowerLayout.addView(textView3);
+        flowerLayout.addView(textView4);
+        flowerLayout.addView(textView5);
+        flowerLayout.addView(textView6);
     }
 }
