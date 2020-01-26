9af6d7944e4f39789865a38011585777deb0f053
tqw214 <tqw214@163.com>
2020-01-26 23:06:01 +0800

    commit

---
+++ binderclient/src/main/java/com/viger/binderclient/eventbus/EventBusActivity.java
@@ -0,0 +1,16 @@
+package com.viger.binderclient.eventbus;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.viger.binderclient.R;
+
+public class EventBusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_bus);
+    }
+}
