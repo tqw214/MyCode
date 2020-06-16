3145d6fc95a4343661dc33cf3537d12276fe49fe
tqw214 <tqw214@163.com>
2020-06-17 00:26:07 +0800

    commit

--- app/src/test/java/com/viger/mycode/TestBean.java
+++ app/src/test/java/com/viger/mycode/TestBean.java
@@ -1,5 +1,7 @@
 package com.viger.mycode;
 
+import java.util.HashSet;
+
 public class TestBean {
 
     private String name = "myname";
@@ -21,6 +23,22 @@ public class TestBean {
     @CheckNet
     public void doSomething(String info) {
         System.out.println("doSomething===>" + info);
+    }
+
+    class Solution {
+        public int lengthOfLongestSubstring(String s) {
+            int res = 0, left = 0, right = 0;
+            HashSet<Character> t = new HashSet<Character>();
+            while (right < s.length()) {
+                if (!t.contains(s.charAt(right))) {
+                    t.add(s.charAt(right++));
+                    res = Math.max(res, t.size());
+                } else {
+                    t.remove(s.charAt(left++));
+                }
+            }
+            return res;
+        }
     }
 
 
