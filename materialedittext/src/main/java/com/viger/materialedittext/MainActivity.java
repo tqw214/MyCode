a517465d92ce40e5e83d07bddbf2f71a9c8bfa30
tqw214 <tqw214@163.com>
2020-08-28 23:03:47 +0800

    commit

---
+++ materialedittext/src/main/java/com/viger/materialedittext/MainActivity.java
@@ -0,0 +1,14 @@
+package com.viger.materialedittext;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ materialedittext/src/main/java/com/viger/materialedittext/MaterialEditText.java
@@ -0,0 +1,4 @@
+package com.viger.materialedittext;
+
+class MaterialEditText {
+}
